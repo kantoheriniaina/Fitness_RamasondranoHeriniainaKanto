32ca3b35655fdf27a87404be45d34656d74434b1
kantoheriniaina <ramasondranoheriniainakanto@gmail.com>
2025-05-21 19:13:46 +0300

    Projet Flutter , Fitness

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.fitness_workout_app_flutter_3_ui_new"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973" // ✅ Fix NDK
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.fitness_workout_app_flutter_3_ui_new"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/fitness_workout_app_flutter_3_ui_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fitness_workout_app_flutter_3_ui_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
